51a410de347f044e985ed301ca251960148849bd
Andrew Kassab <andrewkassab7@gmail.com>
2021-05-31 20:57:41 -0700

    First and second pass test for leapyear program

--- leapyear_unittest.py
+++ leapyear_unittest.py
@@ -2,3 +2,13 @@ import unittest
 import leapyear
 
 class leapyear_test(unittest.TestCase):
+    def test_leapyear_pass(self):
+        result = leapyear.leap_year(2000)
+        self.assertEqual(result,1)
+
+    def test_leapyear_2_pass(self):
+        result = leapyeap.leap_year(2004)
+        self.assertEqual(result,1)
+
+
+
