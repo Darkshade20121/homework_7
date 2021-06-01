a1188b7c98992e176ca3fa0ecd28ed63bee030db
Andrew Kassab <andrewkassab7@gmail.com>
2021-05-31 20:07:38 -0700

    first unittest pass for fizzbuzz program

--- fizzbuzz_unittest.py
+++ fizzbuzz_unittest.py
@@ -2,4 +2,8 @@ import unittest
 import fizzbuzz
 
 class fizzbuzz_test(unittest.TestCase):
+    def test_fizz_pass(self):
+        result = fizzbuzz.fizzbuzz_loop(15)
+        self.assertEqual(result, 1)
 
+    
