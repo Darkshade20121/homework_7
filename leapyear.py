6a8f9853108780dad6e38942bff1b3e42a747dd7
Andrew Kassab <andrewkassab7@gmail.com>
2021-05-31 21:14:36 -0700

    Initial main function without the leap year function

--- leapyear.py
+++ leapyear.py
@@ -6,5 +6,17 @@ def main():
     else:
         print("Is not a leap year")
 
+def leap_year(year):
+    if(year % 4 == 0):
+        if(year % 100 == 0):
+            if(year % 400 == 0):
+                return 1
+            else:
+                return 0
+        else:
+            return 1
+    else:
+        return 0
+
 if __name__ == "__main__":
     main()
